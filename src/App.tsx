f80c62426603d0040b1511ed364077d213cdfe16
Junio Koi <juniokoi@hotmail.com>
2022-06-12 13:37:39 -0300

    convert project from JS to TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import GlobalStyles from "./styles/Global";
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
+
+export default App;
