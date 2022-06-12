73096b138d6486371f4679feedd38922a8dec9ed
Junio Koi <juniokoi@hotmail.com>
2022-06-12 13:38:37 -0300

    Add Header component and style

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "./style";
+
+const Header = () => {
+  return (
+    <>
+      <Container>
+        <h1>Header</h1>a
+      </Container>
+    </>
+  );
+};
+
+export default Header;
